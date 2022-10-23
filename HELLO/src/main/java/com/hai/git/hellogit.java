f76d0119fe8ee6b0591fc8cf74998978e32e7ed0
luolhailong <3559848299@qq.com>
2022-10-23 20:42:43 +0800

    添加主干

--- HELLO/src/main/java/com/hai/git/hellogit.java
+++ HELLO/src/main/java/com/hai/git/hellogit.java
@@ -5,5 +5,6 @@ public class hellogit {
         System.out.println("hello git");
         System.out.println("更新1");
         System.out.println("創建分支");
+        System.out.println("添加主干");
     }
 }
